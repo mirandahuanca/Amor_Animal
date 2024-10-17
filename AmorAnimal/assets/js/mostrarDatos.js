b5781737df9bc9bd26917c8bc4e16fd78eca3807
SofiaSosa1 <127501425+SofiaSosa1@users.noreply.github.com>
2024-10-17 11:52:11 -0300

    Add files via upload

--- AmorAnimal/assets/js/mostrarDatos.js
+++ AmorAnimal/assets/js/mostrarDatos.js
@@ -14,3 +14,19 @@ document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("mostrarEmail").textContent = email;
     document.getElementById("mostrarContraseña").textContent = contraseña;
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    const dropdown = document.querySelector(".dropdown");
+    const content = document.querySelector(".dropdown-content");
+
+    dropdown.addEventListener("click", function() {
+        content.style.display = content.style.display === "block" ? "none" : "block";
+    });
+
+    // Cierra el contenido si se hace clic fuera de él
+    window.addEventListener("click", function(event) {
+        if (!dropdown.contains(event.target)) {
+            content.style.display = "none";
+        }
+    });
+});
